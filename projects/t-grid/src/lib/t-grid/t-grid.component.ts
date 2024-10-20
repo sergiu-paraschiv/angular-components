6dc4f42e01322740c6f093ea6e4414b51ef92fe0
sergiu.paraschiv <sergiuspk@gmail.com>
2024-10-20 15:35:44 +0300

    formatted all source with prettier, removed an unused import

--- projects/t-grid/src/lib/t-grid/t-grid.component.ts
+++ projects/t-grid/src/lib/t-grid/t-grid.component.ts
@@ -1,35 +1,48 @@
-import { Component, ChangeDetectionStrategy, ContentChildren, QueryList, Input, AfterContentInit, AfterContentChecked } from '@angular/core';
-import { TGridRowComponent, TGridColumnDefinition, TGridRowData } from '../t-grid-row/t-grid-row.component';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ContentChildren,
+  QueryList,
+  Input,
+  AfterContentInit,
+  AfterContentChecked,
+} from '@angular/core';
+import {
+  TGridRowComponent,
+  TGridColumnDefinition,
+  TGridRowData,
+} from '../t-grid-row/t-grid-row.component';
 import { TGridHeaderComponent } from '../t-grid-header/t-grid-header.component';
 import { TColumnBase } from './t-column-base';
-
 
 @Component({
   selector: 't-grid',
   standalone: true,
   imports: [TGridHeaderComponent, TGridRowComponent],
   templateUrl: './t-grid.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TGridComponent<T extends TGridRowData> implements AfterContentInit {
-  @Input({required: true}) data: T[] = [];
+export class TGridComponent<T extends TGridRowData>
+  implements AfterContentInit, AfterContentChecked
+{
+  @Input({ required: true }) data: T[] = [];
   @ContentChildren(TColumnBase) private columns!: QueryList<TColumnBase>;
 
   columnDefintions: TGridColumnDefinition[] = [];
 
   ngAfterContentInit() {
-    this.refreshColumnDefinitions(); 
+    this.refreshColumnDefinitions();
   }
 
   ngAfterContentChecked() {
-    this.refreshColumnDefinitions(); 
+    this.refreshColumnDefinitions();
   }
 
   private refreshColumnDefinitions() {
-    this.columnDefintions = this.columns.map(column => ({
+    this.columnDefintions = this.columns.map((column) => ({
       name: column.name,
       property: column.property,
-      sortable: column.sortable
+      sortable: column.sortable,
     }));
   }
 }
